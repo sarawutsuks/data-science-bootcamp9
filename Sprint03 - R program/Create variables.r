9495fc30bedfbc14b3e1c82c018d4477b00b2165
sarawutsuks <156645731+sarawutsuks@users.noreply.github.com>
2024-02-04 11:34:38 +0700

    Create Create variables.r

---
+++ Sprint03 - R program/Create variables.r
@@ -0,0 +1,9 @@
+> income <- 50000  # ให้ income = 50000 ใน R ใช้ <- แทนเครื่องหมาย =
+> income # เรียกดูค่า
+[1] 50000
+> expense <- 30000
+> expense
+[1] 30000
+> saving <- income - expense
+> saving
+[1] 20000
