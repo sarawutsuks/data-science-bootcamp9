55f8decffc6534d99b2e2f8dcc57f08836318409
sarawutsuks <156645731+sarawutsuks@users.noreply.github.com>
2024-02-04 11:36:07 +0700

    Create compare values.r

---
+++ Sprint03 - R program/compare values.r
@@ -0,0 +1,17 @@
+# compare values
+> 1 + 1 == 2   
+[1] TRUE
+> 2 * 2 <= 2
+[1] FALSE
+> 5 >= 10
+[1] FALSE
+> 5-2 != 3
+[1] FALSE
+> 10 < 2
+[1] FALSE
+> 10 > 2
+[1] TRUE
+> 
+# compare text / character
+> "HELLO" == 'hello' # case sensitive
+[1] FALSE
